c55e163e845c066754373b306ef9e2139feda187
Mihail Tsankov <mihail.tsankov@trading212.com>
2025-01-25 11:07:08 +0200

    some changes dk

---
+++ components/exampleComponent1.js
@@ -0,0 +1,34 @@
+import BaseComponent from "./baseComponent.js";
+import { createElement } from "../virtualDom/createRealDOMElement.js";
+
+class ExampleComponent1 extends BaseComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: props.message || "Default Message",
+      showButton: true,
+    };
+  }
+
+  toggleButton = () => {
+    this.setState({ showButton: !this.state.showButton });
+  };
+
+  render() {
+    return createElement(
+      "div",
+      { class: "example-component" },
+      createElement("h1", null, this.props.greeting),
+      createElement("p", null, this.state.message),
+      this.state.showButton
+        ? createElement(
+            "button",
+            { onClick: this.toggleButton },
+            "Toggle Button"
+          )
+        : createElement("h1", null, "Button is hidden")
+    );
+  }
+}
+
+export default ExampleComponent1;
